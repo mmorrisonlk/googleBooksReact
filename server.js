99d394ce3179cb04830343f468a23f08d643c54b
mmorrisonlk <mmorrisonlk@gmail.com>
2021-08-14 16:21:44 -0500

    heroku

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const path = require('path')
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(express.static(path.join(__dirname, 'static')))
 app.use(routes);
 
 mongoose.connect(
